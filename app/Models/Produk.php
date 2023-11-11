f32e7e466d25613043664e778151859108450c2b
khoican <ibnukhoirulprasetyo@gmail.com>
2023-11-11 13:52:29 +0700

    create products table

---
+++ app/Models/Produk.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_produk',
+        'deskripsi',
+        'gambar_1',
+        'gambar_2',
+        'gambar_3',
+        'gambar_4',
+        'harga_beli',
+        'harga_normal',
+        'harga_jual'
+    ];
+}
---
+++ database/migrations/2023_11_11_064331_create_produks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_produk', 50);
+            $table->longtext('deskripsi');
+            $table->string('gambar_1', 50);
+            $table->string('gambar_2', 50);
+            $table->string('gambar_3', 50);
+            $table->string('gambar_4', 50);
+            $table->float('harga_beli');
+            $table->float('harga_normal');
+            $table->float('harga_jual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
