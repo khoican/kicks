75f12be3a11fee5079eb0c759ccff942b2b4440d
khoican <ibnukhoirulprasetyo@gmail.com>
2023-11-11 13:41:30 +0700

    create categories table

--- database/migrations/2023_11_11_063449_create_categories_table.php
+++ database/migrations/2023_11_11_063449_create_categories_table.php
@@ -11,15 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname',50);
-            $table->string('lastname',50);
-            $table->string('gender',2);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password',50);
-            $table->rememberToken();
+            $table->string('kategori', 50);
+            $table->string('deskripsi');
             $table->timestamps();
         });
     }
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
